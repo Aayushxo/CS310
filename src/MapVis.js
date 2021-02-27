7948a65fd968b7da30941501afd9cc8f11f4907b
Aayushxo <asaayush@hotmail.com>
2021-02-27 09:37:50 +0000

    same as before

--- src/MapVis.js
+++ src/MapVis.js
@@ -9,9 +9,10 @@ import rungeKutta from 'runge-kutta';
 
 
 function MapVis() {
-  var data, g, path, S, I, R, colorScale, svg, countryCode, valueWanted, day, newInfection;
+  var data, g, path, S, I, R, colorScale, svg, countryCode, valueWanted;
   //creating an empty dictionary to store SIR model results for each country
   let SIRArray = new Map();
+  let travelArray = []
   let time = 80;
 
   const ref = useRef();
@@ -104,7 +105,10 @@ function MapVis() {
     //array that gets the map and population data
     //object will be used later to see if the retrieval was successful
     let promises = [
+      //get the country data
       d3.json("gadm36_0.json"),
+
+      //get the population data
       d3.csv("population.csv", function (d) {
 
         data.set(d.code, +d.pop);
@@ -112,9 +116,19 @@ function MapVis() {
         //adding the country and the simulation's value's for the country to the dictionary
         SIRArray.set(d.code, SIRModel(0.2, 0.1, d.pop, 100));
 
+        //making a dictionary of countries and corresponding empty array for use later
+        // countryID.set(d.code, [])
+
         //getting SIR values from the user selected parameters
 
+      }),
+      
+      //get the mobility data
+      d3.csv("AirTravel.csv", function (d) {
+       
       })
+
+      
     ]
 
     //if the json and csv files are succesfully retreived, carry out ready()
@@ -124,10 +138,10 @@ function MapVis() {
 
   function ready([topology], countryData) {
 
-
-    //binding the json country data, creating a path for every country
+    //binding the json country data with a path shape, creating a path for every country
     svg.selectAll(".country")
-      .data(topojson.feature(topology, topology.objects.world).features)
+      //getting each country from the json file, whilst filtering out Antarctica
+      .data((topojson.feature(topology, topology.objects.world).features).filter(d => d.properties.iso3 != "ATA"))
       .enter()
       .append("path")
       .attr("class", "country") // give them a class for styling and access later
@@ -142,20 +156,15 @@ function MapVis() {
 
   function sequence() {
 
-    var inf = new Map();
-
-    inf = function () {
-      for (var key in SIRArray) {
-        return SIRArray.get(key)[1]
-      }
-    }
  
     console.log("asd", time);
 
+    //selecting all the countries we bound path shapes to
     d3.selectAll('.country')
       .transition()
       .duration(1000)
       .delay(1000)
+      //setting the colour of each country based on its no. of infections
       .style("fill", function (d) {
 
         //getting the country code from the .csv file
@@ -174,6 +183,28 @@ function MapVis() {
     
 
   }
+
+  const transportSIRModel = (beta, gamma, population, time, connectivity) => {
+    //setting the initial infection as 1
+    //dividing by population to get the percentage of the population that's infected
+    var initInfected = 1 / population;
+
+    // Define the set of ordinary differential equations.
+    const dSIR = (t, y) => [
+      -beta * y[0] * y[1],
+      (beta * y[0] - gamma) * y[1],
+      gamma * y[1]
+    ];
+
+    // Solve the system and log the result (reduced to the infection count).
+    const simulation = rungeKutta(dSIR, [1 - initInfected, initInfected, 0], [0, time], 1);
+
+    S = simulation.map(x => Math.round(x[0] * population));
+    I = simulation.map(x => Math.round(x[1] * population));
+    R = simulation.map(x => Math.round(x[2] * population));
+
+    return [S, I, R];
+  };
 
   return ( <
     div >
